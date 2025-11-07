9ea86c280c407248328610e6c7da9898ebcd948a
Matias Torres <165533228+MatiasITorres@users.noreply.github.com>
2025-11-07 16:49:33 -0300

    Update database configuration with placeholders

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "192.168.101.93";
-$username = "AG08";
-$password = "St2025#QUcwOA";
-$database = "ag08";
+$servername = "your servername";
+$username = "your sername";
+$password = "tu password";
+$database = "your database";
 
-define('DB_HOST', '192.168.101.93');
-define('DB_USER', 'AG08'); // Usuario por defecto de XAMPP
-define('DB_PASS', 'St2025#QUcwOA');     // Contraseña por defecto de XAMPP (vacía)
-define('DB_NAME', 'ag08'); // Nombre de tu base de datos
+define('DB_HOST', 'your servername');
+define('DB_USER', 'your sername'); // Usuario por defecto de XAMPP
+define('DB_PASS', 'tu password');     // Contraseña por defecto de XAMPP (vacía)
+define('DB_NAME', 'your database'); // Nombre de tu base de datos
 
 // Opcional: Configuración para mostrar errores de PHP (solo para desarrollo)
 ini_set('display_errors', 1);
@@ -23,3 +23,4 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 ?>
+
